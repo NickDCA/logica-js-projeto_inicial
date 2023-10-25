74d07be1c6192def9cc5c0132400fac09aa708cc
nicolasdca <nicolasdavid193@gmail.com>
2023-10-25 11:43:37 -0300

    Lógica JS Alura - Aula 3

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 29;
-let chute = prompt('Escolha um número entre 1 e 30');
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
+let chute;
+let tentativas = 1;
 
-if (numeroSecreto === chute) {
-  console.log(`Você descobriu o número secreto: ${numeroSecreto}`);
-} else {
-  console.log('Você errou :(');
+while (chute != numeroSecreto) {
+  chute = prompt('Escolha um número entre 1 e 100');
+  if (numeroSecreto == chute) {
+    break;
+  } else {
+    if (chute > numeroSecreto) {
+      alert(`O número secreto é menor que ${chute}`);
+    } else {
+      alert(`O número secreto é maior que ${chute}`);
+    }
+  }
+  tentativas++;
 }
+
+// if (tentativas > 1) {
+//   alert(
+//     `Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`
+//   );
+// } else {
+//   alert(`Você descobriu o número secreto ${numeroSecreto} com 1 tentativa`);
+// }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+
+alert(
+  `Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`
+);
