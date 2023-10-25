f1bb2bb1d62b10b91b71cd1d864baa6be58cbca2
nicolasdca <nicolasdavid193@gmail.com>
2023-10-25 11:15:28 -0300

    Lógica JS Alura - Aula 2

--- app.js
+++ app.js
@@ -3,5 +3,7 @@ let numeroSecreto = 29;
 let chute = prompt('Escolha um número entre 1 e 30');
 
 if (numeroSecreto === chute) {
-  console.log('Você descobriu o número secreto: ' + numeroSecreto);
+  console.log(`Você descobriu o número secreto: ${numeroSecreto}`);
+} else {
+  console.log('Você errou :(');
 }
